2f1843bf02e8c301125a46d524bb57cbd3a55963
Aartis9 <aartirathore2004@gmail.com>
2024-01-30 20:26:11 +0530

    aa

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom"
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbare';
 import Home from "./container/Home";
 import Resources from './container/Resources';
 import Headnav from "./components/Headnav"
